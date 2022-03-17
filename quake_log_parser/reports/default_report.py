9603b370d84665594f4d170a450dffc6d78d87b5
Milton Castro <castro.milton07@gmail.com>
2022-03-17 03:58:27 -0300

    feat: implement class DefaultReport
    class responsible for maintaining the method to
    generate the default report with matchs informations
    like total kills, players and ranking

--- quake_log_parser/reports/default_report.py
+++ quake_log_parser/reports/default_report.py
@@ -1,7 +1,18 @@
-from quake_log_parser.reports.report import Report
+from reports.report import Report
 
 
 class DefaultReport(Report):
-    def generate(imported_log):
-        report = list()
-        return report
+    def generate(ranking_dict):
+        for index, match in enumerate(ranking_dict.values()):
+            match_num = index + 1
+            total_kills = match["total_kills"]
+            players = match["players"]
+            scores = match["kills"]
+            print("--------------------------------------")
+            print(f" Match: {match_num}")
+            print(f" Total Kills in Match: {total_kills}")
+            print(" Players: ", end="")
+            print(*players, sep=", ")
+            print(" Ranking:")
+            for player, score in scores.items():
+                print(f"    {player}: {score}")
